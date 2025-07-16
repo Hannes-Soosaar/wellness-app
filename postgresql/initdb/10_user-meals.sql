64b921e51d84d65e09ea7c7e2444732df4bf4702
Hannes <hsoosaar@gmail.com>
2025-07-16 17:08:20 +0300

    WIP DB init in a docker container.

--- postgresql/initdb/10_user-meals.sql
+++ postgresql/initdb/10_user-meals.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS user_meals (
+CREATE TABLE IF NOT EXISTS user_meals(
     id SERIAL PRIMARY KEY,
     user_id UUID NOT NULL,
-    meal_type_id TEXT NOT NULL,
+    meal_type_id INT NOT NULL,
     calories INT NOT NULL,
     note TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (meal_type_id) REFERENCES meals(id) ON DELETE CASCADE
 );
--- postgresql/initdb/13_user_meals.sql
+++ postgresql/initdb/13_user_meals.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_meals (
     id SERIAL PRIMARY KEY,
-    user_id UUID NOT NULL
+    user_id UUID NOT NULL,
     meal_id INT NOT NULL,
     calories NUMERIC NOT NULL,
     created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
